b31bc2c38dd1f4ee85f5e2951d65c3eb3cf1a07d
Hamesh Kumar Sharma <32076413+SharmaHK@users.noreply.github.com>
2020-03-29 14:31:41 -0700

    Update BRC9000.py
    The devs of Bannerlord decided to release the game a day earlier. So I've updated the logic accordingly.

--- BRC9000.py
+++ BRC9000.py
@@ -15,7 +15,7 @@ print("Is Mount & Blade II: Bannerlord out yet?")
 print("Today's date is " + date_string + ".")	
 print("Calculating...") #keeps the user at the edge of their seat
 
-if curyear == 20 and curmon == 3 and curday == 31:
+if curyear == 20 and curmon == 3 and curday == 30:
     print ("YES POGGERS")
 elif curyear == 20 and curmon >= 4:
     print ("YES POGGERS")
