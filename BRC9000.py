2415bf696f4fb8b275756fb33f6305258d973f4e
Hamesh Kumar Sharma <32076413+SharmaHK@users.noreply.github.com>
2020-03-29 11:56:16 -0700

    Update BRC9000.py

--- BRC9000.py
+++ BRC9000.py
@@ -3,6 +3,7 @@ from datetime import datetime
 # gives date AND time
 now = datetime.now()
 
+# we only need the date
 curmon = int(now.strftime ("%m"))
 curday = int(now.strftime ("%d"))
 curyear = int(now.strftime ("%y"))
@@ -12,13 +13,13 @@ date_string = now.strftime("%m/%d/%Y")
 
 print("Is Mount & Blade II: Bannerlord out yet?")
 print("Today's date is " + date_string + ".")	
-print("Calculating...")
+print("Calculating...") #keeps the user at the edge of their seat
 
 if curyear == 20 and curmon == 3 and curday == 31:
     print ("YES POGGERS")
 elif curyear == 20 and curmon >= 4:
     print ("YES POGGERS")
 elif curyear > 20:
-    print ("YES POGGERS")
+    print ("YES! It's BEEN out!")
 else: 
     print ("no :(")
